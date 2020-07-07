ccef0e8d0d6bc2d9e430327399ce2c86548c1469
Sergio Ricardo <sergioricardo.ti@gmail.com>
2020-07-07 15:57:17 -0300

    Produto e associacao muitos para muitos

--- src/main/java/br/com/solene/cursomc/domain/Produto.java
+++ src/main/java/br/com/solene/cursomc/domain/Produto.java
@@ -8,33 +8,41 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-//Fazendo o mapeamento da classe categoria
-@Entity
-public class Categoria implements Serializable { // 7 - Implementar a interface Serializable na Classe
-	private static final long serialVersionUID = 1L;
 
+@Entity
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id; // 1 - Atributos básicos
+	private Integer id;
 	private String nome;
-
+	private Double preco;
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-
-	public Categoria() { // 2 - Criando um construtor vazio
-
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", 
+		joinColumns = @JoinColumn(name = "produto_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+			)
+	private List<Categoria> categorias = new ArrayList<>();
+	
+	public Produto() {
+		
 	}
 
-	public Categoria(Integer id, String nome) { // 3 - Criando o construtor com os atributos
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
-	public Integer getId() { // 4 - Getters and Setters
+	public Integer getId() {
 		return id;
 	}
 
@@ -50,23 +58,31 @@ public class Categoria implements Serializable { // 7 - Implementar a interface 
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
-	public int hashCode() { // 5 - Criando o HashCode do campo ID
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
-	@Override // 6 - Criando o Equals do campo ID
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -74,7 +90,7 @@ public class Categoria implements Serializable { // 7 - Implementar a interface 
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -82,5 +98,7 @@ public class Categoria implements Serializable { // 7 - Implementar a interface 
 			return false;
 		return true;
 	}
+	
+	
 
 }
